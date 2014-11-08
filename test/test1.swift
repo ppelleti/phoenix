d5583b5fe3f8c7832c145646d724a492f72dc763
Gregory Casamento <greg.casamento@gmail.com>
2014-11-08 02:27:43 -0500

    Latest changes.

---
+++ test/test1.swift
@@ -0,0 +1,12 @@
+import Foundation;
+
+func test1(iker mortimer:String, eder:Int) -> Int {
+    var team = ["Javi", "Mortimer", "Karliky", "Gorka", "Carlos", "Pablo"];
+    
+    //sort(team) {  $0 > $1 }
+    var index:Int = 0;
+    
+    println("Total members:" + String(index), "aaa");
+    
+    return 0;
+}
---
+++ test/test2.swift
@@ -0,0 +1,27 @@
+func mortimer(let yehaaa javi:String,karliky:Int) -> Dictionary<String,Int > {
+    
+    var index = karliky, values = ["Maldini", "Nesta", "Cannavaro", javi];
+    index++
+    index+=1
+    index--
+    --index
+    let test;
+    test = index + 1 * (1 - (index++ * 4))
+    
+    if index > 2 {
+        return [:]
+    }
+    else if index == 1 {
+        return ["Higuita":80, "Buffon":90, "Chilavert":80]
+    }
+    
+    myLabel:
+    while let (aa,bb) = (true, false) {
+        index = 0;
+        break myLabel;
+    }
+    
+    return index < 0 ? [:] : ["Puskas":95, "Romario":95, "Torres":10]
+}
+
+mortimer(yehaaa:"trololo", 2);
