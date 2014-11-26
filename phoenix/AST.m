0cd19178f4a4d1e352fcfbc86b4f158a3c0a5c7d
Gregory Casamento <greg.casamento@gmail.com>
2014-11-26 03:09:06 -0500

    VariableDeclaration implementation.

--- phoenix/AST.m
+++ phoenix/AST.m
@@ -951,7 +951,33 @@ NSString *tabulate(NSString *code)
 
 - (NSString *) toCode
 {
-    return nil;
+    
+    NSString *result = [self.initializer toCode];
+    
+    //export symbols and vars
+    ExpressionList *node = self.initializer;
+    ExpressionList *item = nil;
+    while ((item = node) != nil)
+    {
+        BinaryExpression *binaryExpr = (BinaryExpression *)(AS([item current], [BinaryExpression class]));
+        if (binaryExpr)
+        {
+            [self exportSymbols:[binaryExpr current]];
+        }
+        else
+        {
+            [self exportSymbols:[item current]];
+        }
+        node = item.next;
+    }
+    
+    //avoid var if exporting variables
+    if (self.exportVariables)
+    {
+        return result;
+    }
+
+    return [NSString stringWithFormat:@"id %@", result];
 }
 
 @end
