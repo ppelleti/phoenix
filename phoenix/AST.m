277d268fd622a8fe55442a7cc75780b5f8e7487d
Gregory Casamento <greg.casamento@gmail.com>
2014-11-26 03:30:44 -0500

    ArrayLiteral implementation.

--- phoenix/AST.m
+++ phoenix/AST.m
@@ -984,18 +984,38 @@ NSString *tabulate(NSString *code)
 
 @implementation ArrayLiteral: ASTNode
 
-
 - (id) initWithItems: (ASTNode *)items
 {
-    return nil;
+    self = [super init];
+    if(self)
+    {
+        self.items = items;
+    }
+    return self;
 }
+
 - (NSString *)toCode
 {
-    return nil;
+    NSString *result = @"[";
+    ASTNode *data = self.items;
+    if(data != nil)
+    {
+        result = [result stringByAppendingString:[data toCode]];
+    }
+    return result;
 }
+
 - (GenericType *)inferType
 {
-    return nil;
+    ExpressionList *node = (ExpressionList *)(AS(self.items,
+                                                 [ExpressionList class]));
+    ASTNode *item = [node current];
+    if(item)
+    {
+        return [[ArrayType alloc] initWithInnerType:[item getType]];
+    }
+    return [[ArrayType alloc] initWithInnerType:
+            [[GenericType alloc] initWithType:TYPE_UNKNOWN]];
 }
 @end
 
